0b4570eb17644f10f05db2d3b65c085746cafc57
Jakub Nowak <nowakkuba99@gmail.com>
2024-06-15 17:03:45 +0200

    Initial Commit

---
+++ metal-playground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  metal-playground
+//
+//  Created by Jakub Nowak on 15/06/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
